b8f3af28b8e37dc66604773ad7903546c63f053f
Tranivic <victor.trani1@gmail.com>
2023-06-15 15:48:50 -0300

    form logic updated

--- js/main.js
+++ js/main.js
@@ -38,7 +38,6 @@ function submitName(event) {
     userName = newUserName;
     setUsernameOnScreen();
 }
-
 function checkUser() {
     const localStorageName = readLocalStorageData('userName');
 
@@ -52,13 +51,11 @@ function checkUser() {
     }
 
 }
-
 function setModalVisibility(isVisible) {
     const modal = document.getElementById('modal');
     modal.classList.toggle('show', isVisible);
     modal.classList.toggle('hidden', !isVisible);
 }
-
 function setUsernameOnScreen() {
     displayedName.innerText = userName;
 }
@@ -78,19 +75,114 @@ function setSubjectObj() {
     let subjectAverage = null;
     let subjectStatusMsg = '';
 
-
     if (isAllScoresFilled) {
-
         subjectAverage = ((N1 * 1.5) + (N2 * 1.5) + (N3 * 4) + (N4 * 3)) / 10;
 
         if (subjectAverage >= 7) {
-            subjectStatusMsg = 'Você passou nessa matéria. Atingiu todos os pontos necessários.';
+            subjectStatusMsg = subjectAverage;
         } else {
-            subjectStatusMsg = 'Você não passou nessa matéria. Não atingiu todos os pontos necessários.';
+            subjectStatusMsg = subjectAverage;
         }
 
+        console.log(subjectStatusMsg);
     } else {
-        const minimumScoreNeeded = 'Waiting...';
-        subjectStatusMsg = `Você precisa obter no mínimo ${minimumScoreNeeded} para passar na matéria.`;
+        const scoreFilledAtTheMoment = [];
+        const missingScoreAtTheMoment = [];
+
+        scores.forEach((element, index) => {
+            if (!isNaN(element)) {
+                if (index === 0 || index === 1) {
+                    scoreFilledAtTheMoment.push(element * 1.5);
+                } else if (index === 2) {
+                    scoreFilledAtTheMoment.push(element * 4);
+                } else {
+                    scoreFilledAtTheMoment.push(element * 3);
+                }
+            } else {
+                missingScoreAtTheMoment.push(`N${index + 1}`);
+            }
+        });
+
+        const totalScore = scoreFilledAtTheMoment.reduce((total, number) => total + number, 0);
+        const missingScoreTotal = Math.ceil(Math.abs(totalScore - 70));
+
+        console.log(scoreFilledAtTheMoment);
+        console.log(missingScoreAtTheMoment);
+        console.log(totalScore);
+        console.log(missingScoreTotal);
+
+        if (missingScoreAtTheMoment.length === 1) {
+            missingScoreAtTheMoment.forEach((element) => {
+                if (element === 'N1') {
+                    console.log(Math.ceil(missingScoreTotal / 1.5));
+                }
+                if (element === 'N2') {
+                    console.log(Math.ceil(missingScoreTotal / 1.5));
+                }
+                if (element === 'N3') {
+                    console.log(missingScoreTotal / 4);
+                }
+                if (element === 'N4') {
+                    console.log(missingScoreTotal / 3);
+                }
+            });
+        } else {
+            if (missingScoreAtTheMoment.includes('N1') && missingScoreAtTheMoment.includes('N2')) {
+                const leftScore = ((missingScoreTotal / 1.5) / 2).toFixed(1);
+                console.log(`Você precisa tirar pelo menos ${leftScore} na N1 e N2`);
+            }
+            if (missingScoreAtTheMoment.includes('N3') && missingScoreAtTheMoment.includes('N4')) {
+                let leftScore = Math.ceil(missingScoreTotal / 2);
+                let leftScoreN3Hold = Math.ceil(leftScore / 4);
+
+                if (leftScoreN3Hold > 10) {
+                    leftScore += (leftScoreN3Hold - 10) * 4;
+                    leftScoreN3Hold = 10;
+                }
+
+                let leftScoreN4Hold = Math.ceil(leftScore / 3);
+
+                console.log(`Você precisa tirar pelo menos ${leftScoreN3Hold} na N3.`);
+                console.log(`Você precisa tirar pelo menos ${leftScoreN4Hold} na N4.`);
+            }
+            if (missingScoreAtTheMoment.includes('N1') && missingScoreAtTheMoment.includes('N3')) {
+                let leftScore = Math.ceil(missingScoreTotal / 2);
+                let leftScoreN1Hold = Math.ceil(leftScore / 1.5);
+
+                if (leftScoreN1Hold > 10) {
+                    leftScore += leftScoreN1Hold - 10;
+                    leftScoreN1Hold = 10;
+                }
+
+                let leftScoreN3Hold = Math.ceil(leftScore / 4);
+
+
+                console.log(`Você precisa tirar pelo menos ${leftScoreN1Hold - 2} na N1.`);
+                console.log(`Você precisa tirar pelo menos ${leftScoreN3Hold + 1} na N3.`);
+            }
+            if (missingScoreAtTheMoment.includes('N2') && missingScoreAtTheMoment.includes('N4')) {
+                let leftScore = Math.ceil(missingScoreTotal / 2);
+                let leftScoreN2Hold = Math.ceil(leftScore / 1.5);
+
+                if (leftScoreN2Hold > 10) {
+                    leftScore += leftScoreN2Hold - 10;
+                    leftScoreN2Hold = 10;
+                }
+
+                let leftScoreN4Hold = Math.ceil(leftScore / 3);
+
+
+                console.log(`Você precisa tirar pelo menos ${leftScoreN2Hold - 2} na N2.`);
+                console.log(`Você precisa tirar pelo menos ${leftScoreN4Hold + 1} na N4.`);
+            }
+
+            if (missingScoreAtTheMoment.includes('N1') && missingScoreAtTheMoment.includes('N4')) {
+
+            }
+            if (missingScoreAtTheMoment.includes('N2') && missingScoreAtTheMoment.includes('N3')) {
+
+            }
+        }
     }
 }
+
